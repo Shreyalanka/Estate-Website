8fa7e6eb563737c9076c424ab4d9d5d5b41e8fc2
Shreyalanka <lankashreya19@gmail.com>
2024-04-29 18:09:11 +0530

    create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import listingRoutes from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -23,8 +24,9 @@ app.listen(3000, () => {
     }
 )
 
-app.use('/api/user', userRoutes)
-app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/listing', listingRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
